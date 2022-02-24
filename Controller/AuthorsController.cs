eaf3795651267e165046ef4393046b7dabab9299
ASCENDER\yiuyeung.Ng <yiuyeung.ng@ceridian.com>
2022-02-24 12:31:28 +1030

    Add project files.

---
+++ Controller/AuthorsController.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using CourseLibrary.API.Models;
+using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CourseLibrary.API.Controller
+{
+    [ApiController]
+    [Route("api/authors")]
+
+    // also see https://docs.microsoft.com/en-us/aspnet/core/web-api/?view=aspnetcore-6.0
+    public class AuthorsController : ControllerBase
+    {
+        private readonly ICourseLibraryRepository _courseLibraryRepository;
+        private readonly IMapper _mapper;
+        public AuthorsController(ICourseLibraryRepository courseLibraryRepository, IMapper mapper)
+        {
+            // check if null
+            _courseLibraryRepository = courseLibraryRepository ??
+                throw new ArgumentNullException(nameof(courseLibraryRepository));
+
+            _mapper = mapper ??
+                throw new ArgumentNullException(nameof(mapper));
+        }
+
+        [HttpGet()]
+        public ActionResult<IEnumerable<AuthorDto>> GetAuthors()
+        {
+            var authorsFromRepo = _courseLibraryRepository.GetAuthors();
+
+            // Map<Type expected>(source)
+            return Ok(_mapper.Map<IEnumerable<AuthorDto>>(authorsFromRepo));
+
+        }
+
+        [HttpGet("{authorId}")]
+        public IActionResult GetAuthor(Guid authorId)
+        {
+            var authorFromRepo = _courseLibraryRepository.GetAuthor(authorId);
+
+            if (authorFromRepo == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(_mapper.Map<AuthorDto>(authorFromRepo));
+        }
+    }
+}
---
+++ Controller/CoursesController.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace CourseLibrary.API.Controller
+{
+    [ApiController]
+    public class CoursesController : ControllerBase
+    {     
+        private readonly ICourseLibraryRepository _courseLibraryRepository;
+        private readonly IMapper _mapper;
+        public CoursesController(ICourseLibraryRepository courseLibraryRepository, IMapper mapper)
+        {
+            // check if null
+            _courseLibraryRepository = courseLibraryRepository ??
+                throw new ArgumentNullException(nameof(courseLibraryRepository));
+
+            _mapper = mapper ??
+                throw new ArgumentNullException(nameof(mapper));
+        }
+    }
+}
